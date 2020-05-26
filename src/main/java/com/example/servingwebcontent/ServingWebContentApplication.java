0bf6ad55c04e19f1204527a10630c59e4d853a13
5schultz <5schultz@informatik.uni-hamburg.de>
2020-05-26 18:42:11 +0200

    test commit

---
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -0,0 +1,17 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
+
+public class ServingWebContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContentApplication.class, args);
+	}
+
+}
