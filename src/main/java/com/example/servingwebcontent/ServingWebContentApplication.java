070e32c259dcdb0939db736ad055d7c737762a4d
velcorn <iwishyouperish@gmail.com>
2020-06-16 23:31:09 +0200

    Database connection doesn't work on Tomcat server...

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -8,11 +8,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 
-
 public class ServingWebContentApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.config.name", "database");
 		SpringApplication.run(ServingWebContentApplication.class, args);
 	}
 }
