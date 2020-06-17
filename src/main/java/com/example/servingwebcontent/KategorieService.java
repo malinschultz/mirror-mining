4af1566ab0333afa47e311b83323366b50cca533
velcorn <iwishyouperish@gmail.com>
2020-06-17 17:24:11 +0200

    Unfixed with db access :P

--- src/main/java/com/example/servingwebcontent/KategorieService.java
+++ src/main/java/com/example/servingwebcontent/KategorieService.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 /*Eine Kategorie ist eine Liste von Artikeln.
@@ -32,7 +33,7 @@ public class KategorieService {
 
     private static void initData() throws JSchException, SQLException, IOException {
         // Get articles from the DB.
-        /*DatabaseConnection db = new DatabaseConnection();
+        DatabaseConnection db = new DatabaseConnection();
         List<Map<String, Object>> documents = db.getData("documents");
 
         Article doc1 = new Article((Integer) documents.get(0).get("id"), documents.get(0).get("title").toString(),
@@ -42,7 +43,7 @@ public class KategorieService {
         Article doc3 = new Article((Integer) documents.get(2).get("id"), documents.get(2).get("title").toString(),
                 documents.get(2).get("comment_tone"), documents.get(2).get("url").toString());
         Article doc4 = new Article((Integer) documents.get(3).get("id"), documents.get(3).get("title").toString(),
-                documents.get(3).get("comment_tone"), documents.get(3).get("url").toString());*/
+                documents.get(3).get("comment_tone"), documents.get(3).get("url").toString());
         Article doc5 = new Article(111, "Irgendein Titel", "0.123", "linkzumartikel.de");
         Article doc6 = new Article(222, "Irgendein Titel", "0.456", "linkzumartikel.de");
         Article doc7 = new Article(333, "Irgendein Titel", "0.789", "linkzumartikel.de");
@@ -51,10 +52,10 @@ public class KategorieService {
         List<Article> kat1 = new ArrayList<>();
         List<Article> kat2 = new ArrayList<>();
 
-        /*kat1.add(doc1);
+        kat1.add(doc1);
         kat1.add(doc2);
         kat2.add(doc3);
-        kat2.add(doc4);*/
+        kat2.add(doc4);
         kat1.add(doc5);
         kat1.add(doc6);
         kat2.add(doc7);
