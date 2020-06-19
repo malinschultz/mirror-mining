133c457cb46b4401b48a65d793cfa4d5064dfaa9
Velcorn <iwishyouperish@gmail.com>
2020-06-19 23:54:28 +0200

    Get categories and documents from DB and feed them to server.

--- src/main/java/com/example/servingwebcontent/Article.java
+++ src/main/java/com/example/servingwebcontent/Article.java
@@ -7,32 +7,36 @@ package com.example.servingwebcontent;
 public class Article {
 
     private int id;
+    private String url;
     private String title;
-    private Object tone;
-    private String link;
+    private String category;
+    private Object comment_tone;
+    private Object answer_tone;
 
-    public Article(int aid, String atitle, Object atone, String alink){
-        this.id=aid;
-        this.title=atitle;
-        this.tone=atone;
-        this.link=alink;
+    public Article(int id, String url, String title, String category, Object ctone, Object atone) {
+        this.id=id;
+        this.url=url;
+        this.title=title;
+        this.category=category;
+        this.comment_tone=ctone;
+        this.answer_tone=atone;
     }
 
     public int getId() {
         return id;
     }
 
+    public String getUrl() { return url; }
+
     public String getTitle() {
         return title;
     }
 
-    public Object getTone() {
-        return tone;
+    public String getCategory() { return category; }
+
+    public Object getCTone() { return comment_tone; }
+
+    public Object getATone() {
+        return answer_tone;
     }
-
-    public String getLink() {
-        return link;
-    }
-
-
 }
--- src/main/java/com/example/servingwebcontent/KategorieService.java
+++ src/main/java/com/example/servingwebcontent/KategorieService.java
@@ -32,40 +32,29 @@ public class KategorieService {
     }
 
     private static void initData() throws JSchException, SQLException, IOException {
-        // Get articles from the DB.
+        // Get categories and documents from the DB.
         DatabaseConnection db = new DatabaseConnection();
+        List<Map<String, Object>> categories = db.getData("categories");
         List<Map<String, Object>> documents = db.getData("documents");
 
-        Article doc1 = new Article((Integer) documents.get(0).get("id"), documents.get(0).get("title").toString(),
-                documents.get(0).get("comment_tone"), documents.get(0).get("url").toString());
-        Article doc2 = new Article((Integer) documents.get(1).get("id"), documents.get(1).get("title").toString(),
-                documents.get(1).get("comment_tone"), documents.get(1).get("url").toString());
-        Article doc3 = new Article((Integer) documents.get(2).get("id"), documents.get(2).get("title").toString(),
-                documents.get(2).get("comment_tone"), documents.get(2).get("url").toString());
-        Article doc4 = new Article((Integer) documents.get(3).get("id"), documents.get(3).get("title").toString(),
-                documents.get(3).get("comment_tone"), documents.get(3).get("url").toString());
-        Article doc5 = new Article(111, "Irgendein Titel", "0.123", "linkzumartikel.de");
-        Article doc6 = new Article(222, "Irgendein Titel", "0.456", "linkzumartikel.de");
-        Article doc7 = new Article(333, "Irgendein Titel", "0.789", "linkzumartikel.de");
-        Article doc8 = new Article(444, "Irgendein Titel", "0.666", "linkzumartikel.de");
+        List<Article> articleList = new ArrayList<>();
+        for (Map<String, Object> document : documents) {
+            Article doc = new Article((Integer) document.get("id"), document.get("url").toString(),
+                    document.get("title").toString(), document.get("category").toString(),
+                    document.get("comment_tone"), document.get("answer_tone"));
+            articleList.add(doc);
+        }
 
-        List<Article> kat1 = new ArrayList<>();
-        List<Article> kat2 = new ArrayList<>();
-
-        kat1.add(doc1);
-        kat1.add(doc2);
-        kat2.add(doc3);
-        kat2.add(doc4);
-        kat1.add(doc5);
-        kat1.add(doc6);
-        kat2.add(doc7);
-        kat2.add(doc8);
-
-        Kategorie langweilig = new Kategorie(kat1, "langweilig");
-        Kategorie spannend = new Kategorie(kat2, "spannend");
-
-        kat.add(langweilig);
-        kat.add(spannend);
+        for (Map<String, Object> category : categories) {
+            List<Article> cat = new ArrayList<>();
+            for (Article doc : articleList) {
+                if (doc.getCategory().equals(category.get("name").toString())) {
+                    cat.add(doc);
+                }
+            }
+            Kategorie c = new Kategorie(cat, category.get("name").toString());
+            kat.add(c);
+        }
     }
 
     public static List<Kategorie> getKat() {
