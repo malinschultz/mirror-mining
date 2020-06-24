09be109cc41150a58abb950a748e58d68c323467
counteachbreath <oliver.gatnar99@web.de>
2020-06-24 15:24:26 +0200

    added charts

--- src/main/resources/static/article-chart.js
+++ src/main/resources/static/article-chart.js
@@ -9,6 +9,12 @@ var chart = new Chart(myChartObject, {
             backgroundColor: 'rgba(255,0,0,0.4)',
             borderColor: 'rgba(255,0,0,1)',
             data: [0.781239 , 0, 0.702673, 0.583166, 0.587752, 0.581369, 0.760855]
+        },{
+            label: "average tone all categories",
+            fill: true,
+            backgroundColor: 'rgba(32,189,57,0.48)',
+            borderColor: 'rgba(32,189,57,1)',
+            data: [0.53495, 0.89323, 0.69058, 0.54758, 0.31234, 0.781923, 0.82012]
         }]
     },
     options: {
