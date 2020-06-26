35b326bf59ccbf61a6c7286f56999f98735c4bbf
5schultz <5schultz@informatik.uni-hamburg.de>
2020-06-26 12:00:20 +0200

    user class added, userservice class added

---
+++ src/main/java/com/example/servingwebcontent/UserService.java
@@ -0,0 +1,43 @@
+package com.example.servingwebcontent;
+
+import com.jcraft.jsch.JSchException;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/*der UserService zieht die Daten aus der DB*/
+public class UserService {
+    private static final List<String> usernames= new ArrayList<String>();
+
+    static {
+        try {
+            initData();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void initData() throws JSchException, SQLException, IOException {
+        // Get users from the DB.
+        DatabaseConnection db = new DatabaseConnection();
+        List<Map<String, Object>> users = db.getData("users");
+        List<Integer> userList = new ArrayList<>();
+        for (Map<String, Object> user : users) {
+            int id = (Integer) user.get("id");
+            userList.add(id);
+        }
+        Collections.sort(userList);
+        for (int i : userList
+        ) {
+            usernames.add(String.valueOf(i));
+        }
+    }
+
+    public static List<String> getUsernames() {
+        return usernames;
+    }
+}
--- src/main/java/com/example/servingwebcontent/controller/UserDetailController.java
+++ src/main/java/com/example/servingwebcontent/controller/UserDetailController.java
@@ -1,15 +1,25 @@
 package com.example.servingwebcontent.controller;
 
+import com.example.servingwebcontent.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class UserDetailController {
     @GetMapping(value = "/userDetail")
     public String userDetail(@RequestParam(name = "userDetail", required = false, defaultValue = "Username")String name, Model model){
         model.addAttribute("username", name);
+
+        /*Hier werden die Daten für die Charts vom user an das Template übergeben
+         * TODO userData passend zu userName */
+        User person = new User();
+        List<Double> data = person.getUserData();
+        model.addAttribute("data", data);
+
         return "userDetail";
     }
 }
