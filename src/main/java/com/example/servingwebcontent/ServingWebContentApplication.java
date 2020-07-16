b4e2a8c527e335ee0ad795f78ef19942e0a727ab
velcorn <iwishyouperish@gmail.com>
2020-07-16 16:16:58 +0200

    Added login with user and pass.

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -2,11 +2,10 @@ package com.example.servingwebcontent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 
 public class ServingWebContentApplication extends SpringBootServletInitializer {
 
